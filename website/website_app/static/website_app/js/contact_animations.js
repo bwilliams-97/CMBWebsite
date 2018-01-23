78fb3b6bbf9babb2cbd053c694d2eb5550599243
bwilliams-97 <brw32@cam.ac.uk>
2018-01-23 20:46:51 +0000

    Added random bits

--- website/website_app/static/website_app/js/contact_animations.js
+++ website/website_app/static/website_app/js/contact_animations.js
@@ -10,7 +10,7 @@ $(function () {
 });
 
  $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 400) { // check if user scrolled more than 50 from top of the browser window
+        if ($(document).scrollTop() > 80) { // check if user scrolled more than 50 from top of the browser window
           $(".navbar-fixed-top").css("background-color", "rgba(0,0,0,0.5)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
           $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
